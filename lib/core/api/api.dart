1169aeabe80f1d2455754eb0a831f29039c38de2
ishgupta05 <ishgupta68@gmail.com>
2025-09-04 22:49:01 +0530

    Initial commit

---
+++ lib/core/api/api.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Api_auth extends StatelessWidget {
+  const Api_auth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/core/di/di.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class didi extends StatelessWidget {
+  const didi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
